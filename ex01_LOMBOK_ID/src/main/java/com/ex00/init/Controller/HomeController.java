e9c6c3ed2e6428286e9cc0b5e3b381b3ec96af92
sinhw0006 <sinhw0006@naver.com>
2025-04-24 09:14:00 +0900

    test

---
+++ ex01_LOMBOK_ID/src/main/java/com/ex00/init/Controller/HomeController.java
@@ -0,0 +1,49 @@
+package com.ex00.init.Controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.log4j.Log4j;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@Log4j
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		//logger.info("Welcome home! The client locale is {}.", locale);
+		
+		log.trace("trace LOG TEST");
+		log.debug("debug LOG TEST");
+		log.info("info LOG TEST");
+		log.warn("warn LOG TEST");
+		log.error("error LOG TEST");
+		log.fatal("fatal LOG TEST");
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+}
